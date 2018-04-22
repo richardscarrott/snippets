8079f097333b4de8c548005797f38e56a41d2fe8
Richard Scarrott <riscarrott@googlemail.com>
2018-04-22 15:06:47 +0100

    #6 Wrap snippet in IIFE to prevent leaking variables to the global

--- src/content/content.js
+++ src/content/content.js
@@ -1,21 +1,7 @@
-// chrome.extension.sendMessage({}, function(response) {
-// 	var readyStateCheckInterval = setInterval(function() {
-// 	if (document.readyState === "complete") {
-// 		clearInterval(readyStateCheckInterval);
-
-// 		// ----------------------------------------------------------
-// 		// This part of the script triggers when page is done loading
-// 		console.log("Hello. This message was sent from scripts/inject.js");
-// 		// ----------------------------------------------------------
-
-// 	}
-// 	}, 10);
-// });
-
-chrome.extension.onMessage.addListener(function(message, sender) {
+chrome.extension.onMessage.addListener(function(message) {
   if (message.action === 'run-snippet') {
     const script = document.createElement('script');
-    script.textContent = message.code;
+    script.textContent = `(function() { ${message.code} })();`;
     (document.head || document.documentElement).appendChild(script);
     script.remove();
   }
