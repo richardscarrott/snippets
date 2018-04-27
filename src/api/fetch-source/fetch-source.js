937a1ece34ee231460576ceb97a1f838ffe4a0e5
Richard Scarrott <richard.scarrott@thetrainline.com>
2018-04-27 13:41:09 +0100

    Debug 406 issue

--- src/api/fetch-source/fetch-source.js
+++ src/api/fetch-source/fetch-source.js
@@ -2,102 +2,112 @@ import { normalize, schema } from 'normalizr';
 import { murmur2 } from 'murmurhash-js';
 import uuid from 'uuid/v1';
 
+// var owner = 'Tango';
+// var repo = 'MobileWeb';
+// var api = 'https://github.thetrainline.com/api/v3/';
+// var path = '/src/client/snippets';
+// var branch = 'master';
+// var fullPath = path
+//     ? `repos/${owner}/${repo}/contents/${path}`
+//     : `repos/${owner}/${repo}/contents`;
+//   // 'https://github.thetrainline.com/api/v3"'
+//   new URL(`${fullPath}?ref=${branch}`, api);
+
+// TODO: Improve this, it oddly enough depends on a trailing slash being on v3/...
 const endpoint = (api, owner, repo, path, branch) => {
-  const fullPath = path
-    ? `repos/${owner}/${repo}/contents/${path}`
-    : `repos/${owner}/${repo}/contents`;
-  return new URL(`${fullPath}?ref=${branch}`, api);
+    const fullPath = path ?
+        `repos/${owner}/${repo}/contents/${path}` :
+        `repos/${owner}/${repo}/contents`;
+    return new URL(`${fullPath}?ref=${branch}`, api);
 };
 
-const fetchContents = async (
-  name,
-  id,
-  api,
-  accessToken,
-  owner,
-  repo,
-  path,
-  branch
+const fetchContents = async(
+    name,
+    id,
+    api,
+    accessToken,
+    owner,
+    repo,
+    path,
+    branch
 ) => {
-  const response = await window.fetch(
-    endpoint(api, owner, repo, path, branch),
-    {
-      headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `token ${accessToken}`
-      }
+    const response = await window.fetch(
+        endpoint(api, owner, repo, path, branch), {
+            headers: {
+                Accept: 'application/vnd.github.v3+json',
+                Authorization: `token ${accessToken}`
+            }
+        }
+    );
+    // TODO: Better error handling / messaging
+    if (response.status !== 200) {
+        throw new Error(`Expected 200, received ${response.status}`);
     }
-  );
-  // TODO: Better error handling / messaging
-  if (response.status !== 200) {
-    throw new Error(`Expected 200, received ${response.status}`);
-  }
-  const contents = await response.json();
-  if (Array.isArray(contents)) {
-    return {
-      id,
-      name,
-      content: await Promise.all(
-        contents.map(content =>
-          fetchContents(
-            content.name,
-            content.sha,
-            api,
-            accessToken,
-            owner,
-            repo,
-            content.path,
-            branch
-          )
-        )
-      )
-    };
-  } else if (contents.type === 'file') {
-    return {
-      id: contents.sha,
-      name: contents.name,
-      content: contents.content
-    };
-  }
-  throw new Error(`contents type not supported: '${contents.type}'`);
+    const contents = await response.json();
+    if (Array.isArray(contents)) {
+        return {
+            id,
+            name,
+            content: await Promise.all(
+                contents.map(content =>
+                    fetchContents(
+                        content.name,
+                        content.sha,
+                        api,
+                        accessToken,
+                        owner,
+                        repo,
+                        content.path,
+                        branch
+                    )
+                )
+            )
+        };
+    } else if (contents.type === 'file') {
+        return {
+            id: contents.sha,
+            name: contents.name,
+            content: contents.content
+        };
+    }
+    throw new Error(`contents type not supported: '${contents.type}'`);
 };
 
 const fileSchema = new schema.Entity('files');
 const dirSchema = new schema.Entity('dirs');
-const contentSchema = new schema.Union(
-  {
-    files: fileSchema,
-    dirs: dirSchema
-  },
-  input => (Array.isArray(input.content) ? 'dirs' : 'files')
+const contentSchema = new schema.Union({
+        files: fileSchema,
+        dirs: dirSchema
+    },
+    input => (Array.isArray(input.content) ? 'dirs' : 'files')
 );
 dirSchema.define({
-  content: [contentSchema]
+    content: [contentSchema]
 });
 const sourceSchema = new schema.Entity('sources', {
-  content: contentSchema
+    content: contentSchema
 });
 export const sourceListSchema = new schema.Array(sourceSchema);
 
 const fetchSource = async request => {
-  const contents = await fetchContents(
-    request.name,
-    // NOTE: Github contents api doesn't return the `sha` of
-    // dir at `path`, so having to generate a unique id in client.
-    uuid(),
-    request.api,
-    request.accessToken,
-    request.owner,
-    request.repo,
-    request.path,
-    request.branch
-  );
-  const source = {
-    ...request,
-    content: contents
-  };
-  const normalizedSource = normalize([source], sourceListSchema);
-  return normalizedSource;
+    const contents = await fetchContents(
+        request.name,
+        // NOTE: Github contents api doesn't return the `sha` of
+        // dir at `path`, so having to generate a unique id in client.
+        uuid(),
+        request.api,
+        request.accessToken,
+        request.owner,
+        request.repo,
+        request.path,
+        request.branch
+    );
+    const source = {
+        ...request,
+        content: contents
+    };
+    const normalizedSource = normalize([source], sourceListSchema);
+    return normalizedSource;
 };
 
 export default fetchSource;
--- src/redux/sagas/add-source/add-source.js
+++ src/redux/sagas/add-source/add-source.js
@@ -2,73 +2,73 @@ import { fork, take, call, put, race } from 'redux-saga/effects';
 import parseGithubUrl from 'parse-github-url';
 import fetchSource from '../../../api/fetch-source/fetch-source';
 import {
-  ADD_SOURCE_REQUESTED,
-  ADD_SOURCE_CANCELLED,
-  addSourceSucceeded,
-  addSourceFailed
+    ADD_SOURCE_REQUESTED,
+    ADD_SOURCE_CANCELLED,
+    addSourceSucceeded,
+    addSourceFailed
 } from '../../actions/add-source/add-source';
 
 function* addSource(source) {
-  let response;
-  try {
-    response = yield call(fetchSource, source);
-  } catch (ex) {
-    console.error(ex);
-    yield put(addSourceFailed(source));
-    return;
-  }
-  yield put(addSourceSucceeded(source, response, Date.now()));
+    let response;
+    try {
+        response = yield call(fetchSource, source);
+    } catch (ex) {
+        console.error(ex);
+        yield put(addSourceFailed(source));
+        return;
+    }
+    yield put(addSourceSucceeded(source, response, Date.now()));
 }
 
 function* addSourceWithCancel(source) {
-  yield race([
-    call(addSource, source),
-    take(
-      action =>
-        action.type === ADD_SOURCE_CANCELLED && source.id === action.meta.id
-    )
-  ]);
+    yield race([
+        call(addSource, source),
+        take(
+            action =>
+            action.type === ADD_SOURCE_CANCELLED && source.id === action.meta.id
+        )
+    ]);
 }
 
 const PUBLIC_GITHUB_HOST = 'github.com';
 
 const getApi = (protocol, host) => {
-  return host === PUBLIC_GITHUB_HOST
-    ? `${protocol}//api.${host}`
-    : `${protocol}//${host}/api/v3`;
+    return host === PUBLIC_GITHUB_HOST ?
+        `${protocol}//api.${host}` :
+        `${protocol}//${host}/api/v3/`;
 };
 
 // TODO: Move to util and unit test
 const getSource = ({ id, name, url, accessToken }) => {
-  const {
-    owner,
-    name: repo,
-    protocol,
-    branch,
-    pathname,
-    host
-  } = parseGithubUrl(url);
-  const path = pathname.split(branch)[1];
-  return {
-    id,
-    name,
-    accessToken,
-    url,
-    path,
-    owner,
-    repo,
-    api: getApi(protocol, host),
-    branch
-  };
+    const {
+        owner,
+        name: repo,
+        protocol,
+        branch,
+        pathname,
+        host
+    } = parseGithubUrl(url);
+    const path = pathname.split(branch)[1];
+    return {
+        id,
+        name,
+        accessToken,
+        url,
+        path,
+        owner,
+        repo,
+        api: getApi(protocol, host),
+        branch
+    };
 };
 
 function* watchAddSource() {
-  while (true) {
-    const action = yield take(ADD_SOURCE_REQUESTED);
-    // TODO: Try catch this.
-    const source = getSource(action.payload);
-    yield fork(addSourceWithCancel, source);
-  }
+    while (true) {
+        const action = yield take(ADD_SOURCE_REQUESTED);
+        // TODO: Try catch this.
+        const source = getSource(action.payload);
+        yield fork(addSourceWithCancel, source);
+    }
 }
 
 export default watchAddSource;
