66559821dbf79ebb6b7a80bb7087d903a618e5c8
Ben Sawyer <ben85ts@gmail.com>
2018-10-22 15:44:15 -0400

    change name to on-committed

--- src/background/background.js
+++ src/background/background.js
@@ -3,7 +3,7 @@ import configureStore from '../redux/store/configure-store';
 import initContextMenu from './context-menu/context-menu';
 import initOmnibox from './omnibox/omnibox';
 import openOptionsPage from './utils/open-options-page/open-options-page';
-import initTabOnUpdated from './tab-on-updated/tab-on-updated';
+import initTabOnUpdated from './on-committed/on-committed';
 
 const [store, persistor] = configureStore();
 window.getStore = () => [store, persistor]; // Expose store to options page.
